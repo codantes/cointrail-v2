57df0be5dccbf7ac157b8fae1766ca4410ec28b4
Pranav Kumar <pranavismic@gmail.com>
2022-07-26 17:47:28 +0530

    removed news details because of a bug

--- components/news/NewsCard.js
+++ components/news/NewsCard.js
@@ -4,7 +4,7 @@ const NewsCard = ({name, image, title}) => {
     return ( 
         <section className='bg-blue/[.35] border-4 border-blue rounded-lg my-8 mx-auto w-4/5 md:w-2/5 flex-col items-start font-mono'>
             <img src={image} className='w-full' alt=""  />
-            <Link href={'/news/' + title}>
+            <Link href={'/'}>
             <h1 className='text-2xl text-yellow m-5 hover:underline cursor-pointer'>
                 {title}
             </h1>
